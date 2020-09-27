a5c8a6d881174dfdc4b3cb410ac30550e3783374
plusman <plusmancn@gmail.com>
2020-09-27 09:08:49 +0800

    feat: add transient example

---
+++ src/main/java/basic/_transient/TransientExample.java
@@ -0,0 +1,63 @@
+package basic._transient;
+
+import java.io.*;
+
+/**
+ * basic.transienT
+ *
+ * @author plusman
+ * @since 2020/9/25
+ */
+class Rectangle implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1710022455003682613L;
+    private Integer width;
+    private Integer height;
+    private transient Integer area;
+
+
+
+    public Rectangle (Integer width, Integer height){
+        this.width = width;
+        this.height = height;
+        this.area = width * height;
+    }
+
+    public void setArea(){
+        this.area = this.width * this.height;
+    }
+
+    @Override
+    public String toString(){
+        StringBuffer sb = new StringBuffer(40);
+        sb.append("width : ");
+        sb.append(this.width);
+        sb.append("\nheight : ");
+        sb.append(this.height);
+        sb.append("\narea : ");
+        sb.append(this.area);
+        return sb.toString();
+    }
+}
+
+public class TransientExample{
+    public static void main(String args[]) throws Exception {
+        Rectangle rectangle = new Rectangle(3,4);
+        System.out.println("1.原始对象\n"+rectangle);
+        ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream("rectangle"));
+        // 往流写入对象
+        o.writeObject(rectangle);
+        o.close();
+
+        // 从流读取对象
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("rectangle"));
+        Rectangle rectangle1 = (Rectangle)in.readObject();
+        System.out.println("2.反序列化后的对象\n"+rectangle1);
+        rectangle1.setArea();
+        System.out.println("3.恢复成原始对象\n"+rectangle1);
+        in.close();
+    }
+}
---
+++ src/main/java/basic/bytecode/Main.java
@@ -0,0 +1,15 @@
+package basic.bytecode;
+
+/**
+ * basic
+ *
+ * @author plusman
+ * @since 2020/9/25
+ */
+public class Main {
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
